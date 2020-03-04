418f3b60b7c3e0fb56d144103cc2237f65303ed8
Eugene Ragasa <eragasa@users.noreply.github.com>
2020-03-04 08:32:42 -0500

    push

--- tests/unittests/MultiCell/test__after_first_iteration/dev__MultiCell.py
+++ tests/unittests/MultiCell/test__after_first_iteration/dev__MultiCell.py
@@ -11,20 +11,73 @@ if __name__ == "__main__":
         'fcc2':os.path.join('simulations','fcc2_000_T400_P0')
     }
 
-    obj = MultiCell.initialize_from_pymatmc2_configuration(
+    old_obj = MultiCell.initialize_from_pymatmc2_configuration(
         configuration=configuration
     )
 
-    obj.cells = None
+    old_obj.cells = None
     for simulation_name, simulation_path in simulation_paths.items():
-        obj.add_simulation(
+        
+        old_obj.add_simulation(
             simulation_type=simulation_type,
             simulation_name=simulation_name,
             simulation_path=simulation_path   
         )
 
-    print(obj.cell_names)
-    print(obj.cell_molar_fraction)
-    print(obj.symbols)
-    print(obj.total_molar_fraction)
-    print(obj.phase_molar_fraction)
+    print(old_obj.cell_names)
+    print(old_obj.cell_molar_fraction)
+    print(old_obj.symbols)
+    print(old_obj.total_molar_fraction)
+    print(old_obj.phase_molar_fraction)
+    print(
+        old_obj.simulations['fcc1'].poscar.path,
+        [
+            old_obj.simulations['fcc1'].poscar.get_number_of_atoms(s) for s in old_obj.symbols
+        ],
+        old_obj.simulations['fcc1'].outcar.total_energy
+    )
+    print(
+        old_obj.simulations['fcc2'].poscar.path,
+        [
+            old_obj.simulations['fcc2'].poscar.get_number_of_atoms(s) for s in old_obj.symbols
+        ],
+        old_obj.simulations['fcc2'].outcar.total_energy
+    )
+
+    new_obj = MultiCell.initialize_from_pymatmc2_configuration(
+        configuration=configuration
+    )
+    simulation_paths = {
+        'fcc1':os.path.join('simulations','fcc1_001_T400_P0'),
+        'fcc2':os.path.join('simulations','fcc2_001_T400_P0')
+    }
+    new_obj.cells = None
+    for simulation_name, simulation_path in simulation_paths.items():
+        new_obj.add_simulation(
+            simulation_type=simulation_type,
+            simulation_name=simulation_name,
+            simulation_path=simulation_path   
+        )
+    print(new_obj.cell_names)
+    print(new_obj.cell_molar_fraction)
+    print(new_obj.symbols)
+    print(new_obj.total_molar_fraction)
+    print(new_obj.phase_molar_fraction)
+    print(
+        new_obj.simulations['fcc1'].poscar.path,
+        [
+            new_obj.simulations['fcc1'].poscar.get_number_of_atoms(s) for s in new_obj.symbols
+        ],
+        [
+            new_obj.cells['fcc1'].get_number_of_atoms(s) for s in new_obj.symbols
+        ],
+
+        new_obj.simulations['fcc1'].outcar.total_energy
+    )
+    print(
+        new_obj.simulations['fcc2'].poscar.path,
+        [
+            new_obj.simulations['fcc2'].poscar.get_number_of_atoms(s) for s in new_obj.symbols
+        ],
+        new_obj.simulations['fcc2'].outcar.total_energy
+    )
